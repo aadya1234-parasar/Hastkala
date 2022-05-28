8b3b88682906bf1742c956087839f45ae853b260
djangoadmin <aadyaparasar1234@gmail.com>
2022-05-28 12:55:33 +0530

    allowed host

--- Hastkala/settings.py
+++ Hastkala/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['HastKala-course-env.eba-k7uqa59w.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['HastKala-course-env.eba-k7uqa59w.us-west-2.elasticbeanstalk.com/']
 
 
 # Application definition
